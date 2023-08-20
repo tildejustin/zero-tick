e482d6e8f5d93463cc0d29d6fc80b41449a70d00
tildejustin <spacepepper3.14@gmail.com>
2023-08-20 19:38:35 -0400

    readme update and vines

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zero Tick Crops
+
+A mod that reverts the fix for [MC-113809](https://bugs.mojang.com/browse/MC-113809) and brings back the imfamous 0-tick farms of pre 1.16
---
+++ src/main/java/dev/tildejustin/mixin/VineBlockMixin.java
@@ -0,0 +1,26 @@
+package dev.tildejustin.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.VineBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Random;
+
+@Mixin(VineBlock.class)
+public abstract class VineBlockMixin extends Block {
+    public VineBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Shadow
+    public abstract void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random);
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        this.randomTick(state, world, pos, random);
+    }
+}
